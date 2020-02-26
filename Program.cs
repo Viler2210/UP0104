9bf98509adb62edd27c62db88bbc87bb5fe3b7fe
Viler2210 <viler.lucmanov2016@yandex.ru>
2020-02-26 09:31:15 +0500

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UP0104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
